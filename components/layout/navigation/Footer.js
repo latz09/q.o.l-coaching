c3156f0b879b3a21235b83b525d27dd1559d8e0f
latz09 <85132328+latz09@users.noreply.github.com>
2026-02-17 15:48:55 -0600

    df

--- components/layout/navigation/Footer.js
+++ components/layout/navigation/Footer.js
@@ -19,17 +19,21 @@ const Footer = ({ data }) => {
 		<div className='bg-gradient-to-t lg:bg-gradient-to-r from-primary via-primary/95 to-primary/90'>
 			<div className='px-1.25 lg:px-0.25 pt-4 lg:pt-4 container grid  place-items-center lg:place-items-start lg:text-start text-center'>
 				{logo?.asset?.url && (
-					<Image
-						src={logo.asset.url}
-						alt='Q.O.L.C'
-						height={275}
-						width={275}
-						blurDataURL={logo.asset.metadata?.lqip}
-					/>
+					<Link href='/'>
+						<Image
+							src={logo.asset.url}
+							alt='Q.O.L.C'
+							height={275}
+							width={275}
+							blurDataURL={logo.asset.metadata?.lqip}
+						/>
+					</Link>
 				)}
 				<div ref={textRef} className='lg:w-fit'>
 					<p className='text-paragraph-sm text-white mt-1.5'>{tagline}</p>
-					<p className='text-paragraph-sm text-white mb-1.5 lg:mb-3.75'>{location}</p>
+					<p className='text-paragraph-sm text-white mb-1.5 lg:mb-3.75'>
+						{location}
+					</p>
 				</div>
 				<div className='flex flex-col lg:flex-row items-start lg:items-center justify-between gap-1.5 lg:gap-0 lg:w-full'>
 					<p
@@ -38,7 +42,7 @@ const Footer = ({ data }) => {
 					>
 						{disclaimer}
 					</p>
-					<div className="grid place-items-center w-full lg:w-auto mt-1.25 lg:mt-0 ">
+					<div className='grid place-items-center w-full lg:w-auto mt-1.25 lg:mt-0 '>
 						<Link
 							href='/#contact-form'
 							className='bg-green-light rounded-full px-1.5 py-0.75 text-[1.125rem] font-[600] hover:opacity-90 transition duration-500 hover:text-white'
