5d830e6537c777f760fddd48036198795495fd19
latz09 <85132328+latz09@users.noreply.github.com>
2026-03-19 13:32:16 -0500

    mobile cms guide fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,11 @@
 const SITE_URL = 'https://www.qolc.latzwebdesign.com';
 
 module.exports = {
-	siteUrl: SITE_URL,
-	generateRobotsTxt: true,
-	robotsTxtOptions: {
-		policies: [{ userAgent: '*', allow: '/' }],
-	},
+  siteUrl: SITE_URL,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/', disallow: '/cms-guide' },
+    ],
+  },
 };
